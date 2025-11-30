238d6653fd0cbc6fd26eda4feb38f13db390e57b
cesar-hernando <cesar.hernando22@gmail.com>
2025-11-30 06:05:42 +0100

    documented the code

--- haiqu_partial_ec.py
+++ haiqu_partial_ec.py
@@ -20,7 +20,7 @@ from gate_score_assignment import compute_discounted_lightcone_scores_bitset
 
 def transform_circuit(circ: QuantumCircuit) -> QuantumCircuit:
     """
-    Baseline transformation pass that:
+    Designed transformation pass that:
 
       - Preserves the circuit structure (same ops, same order, same wires),
       - Selects at most one gate per DAG layer to mark as FT,
@@ -35,38 +35,39 @@ def transform_circuit(circ: QuantumCircuit) -> QuantumCircuit:
          with the FT version.
       5. Convert the modified DAG back to a QuantumCircuit.
 
-    This is a simple baseline; participants should implement a better strategy
-    in their own transform_circuit, but it must obey the same rules.
     """
     dag = circuit_to_dag(circ)
     new_dag = dag.copy_empty_like()   
 
-    layers=list(dag.layers())
-    num_qubits=dag.num_qubits()
-    scores=np.zeros(num_qubits)
+    layers = list(dag.layers())
+    num_qubits = dag.num_qubits()
+    scores = np.zeros(num_qubits)
     gates = []
 
-    for i, layer in enumerate(reversed(layers)): #follow reversed order
-        layer_dag=layer['graph']
-        best_node_index=-1
-        best_score=-1
+    # For each layer, select the gate with the maximum influence in future computations
+    for i, layer in enumerate(reversed(layers)): # follow reversed order
+        layer_dag = layer['graph']
+        best_node_index = -1
+        best_score = -1
         current_scores = scores.copy()
         for i, node in enumerate(layer_dag.op_nodes()):
             if getattr(node.op, "name") in ALLOWED_BASE_GATES:
-                score_node= 2.5*len(node.qargs)
+                score_node = 2.5*len(node.qargs)
                 for qubit in node.qargs:
                     score_node += scores[qubit._index]
                 for qubit in node.qargs:
-                    scores[qubit._index]=score_node
+                    scores[qubit._index] = score_node
 
-                if score_node>best_score:
-                    best_score=score_node
-                    best_node_index=i       
+                if score_node > best_score:
+                    best_score = score_node
+                    best_node_index = i       
         gates.append(best_node_index)
         
         scores -= 2/3*current_scores
 
     gates.reverse()
+
+    # Make the estimated most influencial gates fault-tolerant
     for j, layer in enumerate(dag.layers()):
         layer_dag = layer["graph"]
 
@@ -130,6 +131,10 @@ def baseline_transform(circ: QuantumCircuit) -> QuantumCircuit:
 
 if __name__ == '__main__':
 
+    ################################################
+    # 0. Define simulation parameters              #
+    ################################################
+
     np.random.seed(42)
     p_1q = 1e-2   # depolarizing error for 1-qubit native gates
     p_2q = 5e-2   # depolarizing error for 2-qubit native gates
@@ -137,10 +142,18 @@ if __name__ == '__main__':
     test_circuit_type = 'qpe' # 'random', 'qft' or 'qft'
     n_circuits = 10 # number of test circuits
 
+    ################################################
+    # 1. Initialize noise model and simulator      #
+    ################################################
+
     noise_model = build_noise_model(p_1q=p_1q, p_2q=p_2q, ft_scale=ft_scale)
 
     ideal_sim = AerSimulator()
     noisy_sim = AerSimulator(noise_model=noise_model)
+
+    ########################################################################
+    # 2. Utilize random circuits, QFT and QPE for testing                  #
+    ########################################################################
 
     benchmarking = TestCircuits(p_1q=p_1q, p_2q=p_2q, ft_scale=ft_scale)
     if test_circuit_type == 'qft':
@@ -150,8 +163,11 @@ if __name__ == '__main__':
     elif test_circuit_type == 'qpe':
         benchmarking_circuits = benchmarking.get_qpe_circuits(n_circuits)
     
+    ########################################################################
+    # 2. Transform the first test circuit and visualize the result         #
+    ########################################################################
+
     viz_circuit = benchmarking_circuits[0]
-    
     viz_circuit_ft = transform_circuit(viz_circuit)
     viz_circuit_ft_baseline = baseline_transform((viz_circuit))
     
@@ -170,6 +186,11 @@ if __name__ == '__main__':
     fig3 = viz_circuit_ft_baseline.draw(output="mpl")
     fig3.savefig("circuit_ft_baseline.png", dpi=300, bbox_inches="tight")
     
+    ############################################################################
+    # Compare the measured histogram of the ideal circuit with the partial     #
+    # fault-tolerant transformed circuits using the baseline and our algorithm #
+    ############################################################################
+
     def filter_counts(counts, threshold):
         return {k: v for k, v in counts.items() if v >= threshold}
     
@@ -184,41 +205,44 @@ if __name__ == '__main__':
     
     fig3.savefig("histogram.png", dpi=300)
       
-    print("Running grader for baseline transform...")
+    ############################################################################
+    # Evaluate our partial fault-tolerant circuit transformation with the      #
+    # provided grading function and our benchmarking circuits                  #
+    ############################################################################
 
+    def print_results(grade, verbose=True):
+        print("Valid submission?      :", grade["ok"])
+        print("Average improvement    :", grade["average_improvement"])
+
+        if verbose:
+            print("Per-circuit improvement:", grade["improvements"])
+            print("Plain fidelities       :", grade["fidelities_plain"])
+            print("FT fidelities          :", grade["fidelities_ft"])
+
+        if not grade["ok"]:
+            print("\nErrors:")
+            for err in grade["errors"]:
+                print("  -", err)
+
+    # Grade the baseline circuit transformation
+    print("\nRunning grader for baseline circuit transformation...")
     grade = grader(
         transform_circuit_fn=baseline_transform,
         circuits=benchmarking_circuits,
         noise_model=noise_model,
         shots=200_000,   # reduce for demo speed
     )
-
-    print("Valid submission?      :", grade["ok"])
-    print("Average improvement    :", grade["average_improvement"])
-    print("Per-circuit improvement:", grade["improvements"])
-    print("Plain fidelities       :", grade["fidelities_plain"])
-    print("FT fidelities          :", grade["fidelities_ft"])
-
-    if not grade["ok"]:
-        print("\nErrors:")
-        for err in grade["errors"]:
-            print("  -", err)
-            
-    print("Running grader for circuit transform...")
-
+    print_results(grade, verbose=False)
+      
+    # Grade our custom circuit transformation  
+    print("\nRunning grader for our custom circuit transform...")
     grade = grader(
         transform_circuit_fn=transform_circuit,
         circuits=benchmarking_circuits,
         noise_model=noise_model,
         shots=200_000,   # reduce for demo speed
     )
-    print("Valid submission?      :", grade["ok"])
-    print("Average improvement    :", grade["average_improvement"])
-    print("Per-circuit improvement:", grade["improvements"])
-    print("Plain fidelities       :", grade["fidelities_plain"])
-    print("FT fidelities          :", grade["fidelities_ft"])
+    print_results(grade, verbose=False)
 
-    if not grade["ok"]:
-        print("\nErrors:")
-        for err in grade["errors"]:
-            print("  -", err)
+    print("")
+    
